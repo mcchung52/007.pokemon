44a3ad1d9de70e80b9d2c98f4d164d7b3f69d532
Michael Chung <mcchung52@hotmail.com>
2020-06-21 20:11:24 -0700

    select and show all pokemons who name contains the input

--- main.js
+++ main.js
@@ -20,28 +20,30 @@ function init() {
 function getClicked() {
 
 	var pokemonName = $('#input').val();
-	var selectedPokemon = pokemon.filter(function(ob){
-		return ob.pokemon_species.name.toLowerCase() == pokemonName.toLowerCase();
-	})[0];
-
-	var $pokemon = $('<div>').addClass('card');
-	$.ajax(URL + '/pokemon/' + selectedPokemon.entry_number, {
-		success: function(data) {
-			//$.ajax(URL + data.sprites[0].resource_uri, {
-			//	success: function(data) {
-					var $img = $('<img>');
-					$img.attr('src',data.sprites.front_default);
-					$img.css('width','200px');
-					$pokemon.prepend($img);
-			//	}
-			//});
-			var $p1 = $('<p>').text(data.name).addClass('name');
-			var stat = data.stats;
-			var $p2 = $('<p>').text('HP: ' + stat['0'].base_stat);
-			var $p3 = $('<p>').text('Attack: ' + stat['1'].base_stat);
-			var $p4 = $('<p>').text('Defense: ' + stat['2'].base_stat);
-			$pokemon.append($p1,$p2,$p3,$p4);
-		}
+	var selectedPokemons = pokemon.filter(function(ob){
+		return ob.pokemon_species.name.toLowerCase().includes(pokemonName.toLowerCase());
 	});
-	$('#container').append($pokemon);	
+
+	for (var pm in selectedPokemons) {
+		var $pokemon = $('<div>').addClass('card');
+		$.ajax(URL + '/pokemon/' + pm.entry_number, {
+			success: function(data) {
+				//$.ajax(URL + data.sprites[0].resource_uri, {
+				//	success: function(data) {
+						var $img = $('<img>');
+						$img.attr('src',data.sprites.front_default);
+						$img.css('width','200px');
+						$pokemon.prepend($img);
+				//	}
+				//});
+				var $p1 = $('<p>').text(data.name).addClass('name');
+				var stat = data.stats;
+				var $p2 = $('<p>').text('HP: ' + stat['0'].base_stat);
+				var $p3 = $('<p>').text('Attack: ' + stat['1'].base_stat);
+				var $p4 = $('<p>').text('Defense: ' + stat['2'].base_stat);
+				$pokemon.append($p1,$p2,$p3,$p4);
+			}
+		});
+		$('#container').append($pokemon);
+	}	
 }
